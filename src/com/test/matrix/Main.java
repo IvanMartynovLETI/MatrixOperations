c67881905721b4b87d9fb3caef7322740978c4c6
Рымонин Олег <orymonin@systematic.ru>
2019-02-07 12:07:01 +0300

    Рефакторинг метода main

--- src/com/test/matrix/Main.java
+++ src/com/test/matrix/Main.java
@@ -7,87 +7,57 @@ import com.test.matrix.classes.flip.FlipRelToSecDiag;
 import com.test.matrix.classes.flip.FlipVertically;
 import com.test.matrix.classes.generation.GenerateRandomMatrix;
 import com.test.matrix.classes.mapping.DisplayMatrix;
-import com.test.matrix.interfaces.GenerateMatrix;
 import com.test.matrix.interfaces.ShowMatrix;
 import com.test.matrix.interfaces.UserFlip;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Main {
+  public static void main(String[] args) {
+    try {
+      int dim = checkInput(args);
 
-    public static void main(String[] args) {
+      List<Supplier<UserFlip>> algorithms = Arrays.asList(
+          FlipHorizontally::new,
+          FlipVertically::new,
+          FlipRelToMainDiag::new,
+          FlipRelToSecDiag::new,
+          UserClass::new
+      );
 
-        int dim = checkInput(args);
+      ShowMatrix matrixPrinter = new DisplayMatrix();
+      int[][] matrix = new GenerateRandomMatrix().generateMatrix(dim);
 
-        GenerateRandomMatrix genMObj = new GenerateRandomMatrix();
-        FlipHorizontally flipHorObj = new FlipHorizontally();
-        FlipVertically flipVertObj = new FlipVertically();
-        FlipRelToMainDiag flipMDObj = new FlipRelToMainDiag();
-        FlipRelToSecDiag flipSDObj = new FlipRelToSecDiag();
-        DisplayMatrix dispMObj = new DisplayMatrix();
-        UserClass userObj = new UserClass();
+      System.out.println("Initial matrix is");
+      matrixPrinter.displayMatrix(matrix);
 
-        UserFlip userFlipRef;
-        GenerateMatrix genMRef;
-        ShowMatrix showMRef;
+      for (Supplier<UserFlip> alg : algorithms) {
+        UserFlip userFlip = alg.get();
+        System.out.println(userFlip.getDescription());
+        matrixPrinter.displayMatrix(userFlip.flipMethod(matrix));
+      }
 
-        showMRef = dispMObj;
+    } catch (IllegalArgumentException e) {
+      System.out.println("Wrong argument: " + e.getMessage());
+    }
+  }
 
-        genMRef = genMObj;
-
-        int[][] m1 = genMRef.generateMatrix(dim);
-        System.out.println("Initial matrix is");
-        showMRef.displayMatrix(m1);
-        System.out.println(showMRef.getDescription());
-
-        System.out.println("Initial matrix after horizontal flipping is");
-        userFlipRef = flipHorObj;
-        int[][] m2 = userFlipRef.flipMethod(m1);
-        showMRef.displayMatrix(m2);
-        System.out.println(userFlipRef.getDescription());
-
-        System.out.println("Initial matrix after vertical flipping is");
-        userFlipRef = flipVertObj;
-        int[][] m3 = userFlipRef.flipMethod(m1);
-        showMRef.displayMatrix(m3);
-        System.out.println(userFlipRef.getDescription());
-
-        System.out.println("Initial matrix after flipping relative to main diagonal is");
-        userFlipRef = flipMDObj;
-        int[][] m4 = userFlipRef.flipMethod(m1);
-        showMRef.displayMatrix(m4);
-        System.out.println(userFlipRef.getDescription());
-
-        System.out.println("Initial matrix after flipping relative to secondary diagonal is");
-        userFlipRef = flipSDObj;
-        int[][] m5 = userFlipRef.flipMethod(m1);
-        showMRef.displayMatrix(m5);
-        System.out.println(userFlipRef.getDescription());
-
-        System.out.println("Initial matrix after user flipping is");
-        userFlipRef = userObj;
-        int[][] m6 = userFlipRef.flipMethod(m1);
-        showMRef.displayMatrix(m6);
-        System.out.println(userFlipRef.getDescription());
+  private static int checkInput(String[] strArr) {
+    if (strArr.length == 0) {
+      throw new IllegalArgumentException("Empty dimension");
     }
 
-    private static int checkInput(String[] strArr) {
-        int dimension;
-        if (strArr.length == 0) {
-            System.out.println("Command line parameter is empty.");
-            System.out.println("0 as output parameter for checkInput() method generated");
-            dimension = 0;
-        } else {
-            try {
-                dimension = Integer.parseInt(strArr[0]);
-            } catch (NumberFormatException ex) {
-                System.out.println("Command line parameter is not an integer");
-                System.out.println("0 as output parameter for checkInput() method generated");
-                dimension = 0;
-            }
-            //ок, пустой параметр, не целое число и не число мы отловили, что осталось?
-            //мы предполагаем, что пользователь неадекватен и может передать ахинею
-            //частные случаи ахинеи, а именно 0, 1 и отрицательные целые мы будем
-            //отлавливать в методе GenerateRandomMatrix()
-        }
-        return dimension;
+    try {
+      int i = Integer.parseInt(strArr[0]);
+      if (i < 2 || i > 100) {
+        throw new IllegalArgumentException("Dimension must be an in range 2 between 100");
+      }
+      return i;
+    } catch (NumberFormatException ex) {
+      throw new IllegalArgumentException("Dimension must an integer");
     }
+  }
+
 }
