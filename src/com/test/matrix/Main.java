0bea3444b3f7b54787f4541561334be9c11dfc0d
IvanMartynovLETI <iamartynov@rambler.ru>
2019-02-13 19:49:09 +0300

    вторая версия ядра

--- src/com/test/matrix/Main.java
+++ src/com/test/matrix/Main.java
@@ -8,7 +8,6 @@ import com.test.matrix.classes.flip.FlipVertically;
 import com.test.matrix.classes.generation.GenerateRandomMatrix;
 import com.test.matrix.classes.mapping.DisplayMatrix;
 import com.test.matrix.interfaces.GenerateMatrix;
-import com.test.matrix.interfaces.ShowMatrix;
 import com.test.matrix.interfaces.UserFlip;
 
 import java.util.*;
@@ -18,30 +17,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<String> userList = new ArrayList<String>();
-        userList.add("showResult");
-        userList.add("showResult.getDescription");
-        userList.add("generateMatrix.getDescriprion");
-        userList.add("flipVert");
-        userList.add("showResult");
-        userList.add("flipVert.getDescription");
-        userList.add("flipHor");
-        userList.add("showResult");
-        userList.add("flipHor.getDescription");
-        userList.add("flipMD");
-        userList.add("showResult");
-        userList.add("flipMD.getDescription");
-        userList.add("flipSD");
-        userList.add("showResult");
-        userList.add("flipSD.getDescription");
-        userList.add("userFlip");
-        userList.add("showResult");
-        userList.add("userFlip.getDescription");
-        //пока предполагается, что userList формируется внешним по отношению к core кодом
-        //здесь код формирования userList пока не рассматривается.
-        //в дальнейшем его будет формировать отдельный метод, который и отловит косяки ввода команд пользователя
-        core(userList, args);
+        GenerateMatrix genM = new GenerateRandomMatrix();
+        UserFlip ufRef = new FlipVertically();
 
+        ArrayList<UserFlip> algorithms = new ArrayList<UserFlip>();
+        algorithms.add(new FlipHorizontally());
+        algorithms.add(new FlipVertically());
+        algorithms.add(new FlipHorizontally());
+        algorithms.add(new FlipRelToMainDiag());
+        algorithms.add(new FlipRelToSecDiag());
+        algorithms.add(new UserClass());
+
+        try {
+            new DisplayMatrix().displayMatrix(core(algorithms, genM, args));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Wrong argument " + e.getMessage() + "\n");
+        }
     }
 
     private static int checkInput(String[] strArr) {
@@ -60,77 +51,23 @@ public class Main {
         }
     }
 
-    public static void core(ArrayList<String> userList, String[] args) {
-
-        ArrayList<String> acceptedOps = new ArrayList<String>(); //в дальнейшем этот список допустимых
-        acceptedOps.add("flipHor");                           //команд должен формироваться ядром
-        acceptedOps.add("flipVert");                             //самостоятельно
-        acceptedOps.add("flipMD");
-        acceptedOps.add("flipSD");
-        acceptedOps.add("userFlip");
-        acceptedOps.add("showResult");
-        acceptedOps.add("generateMatrix.getDescriprion");
-        acceptedOps.add("showResult.getDescription");
-        acceptedOps.add("flipHor.getDescription");
-        acceptedOps.add("flipVert.getDescription");
-        acceptedOps.add("flipMD.getDescription");
-        acceptedOps.add("flipSD.getDescription");
-        acceptedOps.add("userFlip.getDescription");
-
-        List<UserFlip> algorithms = Arrays.asList(
-                new FlipHorizontally(),
-                new FlipVertically(),
-                new FlipRelToMainDiag(),
-                new FlipRelToSecDiag(),
-                new UserClass()
-        );
-
-        Map<String, UserFlip> flipVoc = new HashMap<String, UserFlip>();
-        flipVoc.put(acceptedOps.get(0), algorithms.get(0));
-        flipVoc.put(acceptedOps.get(1), algorithms.get(1));
-        flipVoc.put(acceptedOps.get(2), algorithms.get(2));
-        flipVoc.put(acceptedOps.get(3), algorithms.get(3));
-        flipVoc.put(acceptedOps.get(4), algorithms.get(4));
-
-        flipVoc.put(acceptedOps.get(8), algorithms.get(0));//вот эта вторая половина словаря мне не нравится,
-        flipVoc.put(acceptedOps.get(9), algorithms.get(1));//но как обойти это место я пока не знаю
-        flipVoc.put(acceptedOps.get(10), algorithms.get(2));
-        flipVoc.put(acceptedOps.get(11), algorithms.get(3));
-        flipVoc.put(acceptedOps.get(12), algorithms.get(4));
+    public static int[][] core(List<UserFlip> algs, GenerateMatrix genM, String[] strArr) {
 
         try {
-            int dim = checkInput(args);
-            ShowMatrix matrixPrinter = new DisplayMatrix();
-            int[][] initialMatrix = new GenerateRandomMatrix().generateMatrix(dim);
-            int[][] tempMatrix = initialMatrix;
 
-            for (int i = 0; i < userList.size(); i++) {
-                if (userList.get(i) == acceptedOps.get(5)) {
-                    new DisplayMatrix().displayMatrix(tempMatrix);
-                } else if (userList.get(i) == acceptedOps.get(6)) {
-                    System.out.println(new GenerateRandomMatrix().getDescription());
-                } else if (userList.get(i) == acceptedOps.get(7)) {
-                    System.out.println(new DisplayMatrix().getDescription());
-                } else {
-                    UserFlip ufRef;
-                    if (userList.get(i) == acceptedOps.get(8) || userList.get(i) == acceptedOps.get(9) ||
-                            userList.get(i) == acceptedOps.get(10) || userList.get(i) == acceptedOps.get(11) ||
-                            userList.get(i) == acceptedOps.get(12)) {
-                        ufRef = flipVoc.get(userList.get(i));
-                        System.out.println(ufRef.getDescription());
-                    } else if (userList.get(i) == acceptedOps.get(0) || userList.get(i) == acceptedOps.get(1) ||
-                            userList.get(i) == acceptedOps.get(2) || userList.get(i) == acceptedOps.get(3) ||
-                            userList.get(i) == acceptedOps.get(4)) {
-                        ufRef = flipVoc.get(userList.get(i));
-                        tempMatrix = ufRef.flipMethod(tempMatrix);
-                    } else {
-                        throw new IllegalArgumentException("Invalid user command found");
-                    }
-                }
+            int dim = checkInput(strArr);
+            int[][] matrix = genM.generateMatrix(dim);
+            System.out.println("Initial matrix is");
+            new DisplayMatrix().displayMatrix(matrix);
+
+            for (UserFlip userFlip : algs) {
+                matrix = userFlip.flipMethod(matrix);
+                System.out.println(userFlip.getDescription());
             }
+            return matrix;
 
-        } catch (IllegalArgumentException e) {
-            System.out.println("Wrong argument " + e.getMessage());
+        } catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException(ex.getMessage());
         }
     }
 }
