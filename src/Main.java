0e53a2f17898913f3974d565dca11f37f2fa671a
IvanMartynovLETI <iamartynov@rambler.ru>
2019-03-26 20:55:35 +0300

    yaml config improved

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,9 @@
-import com.test.matrix.classes.calculator.Calculator;
-import com.test.matrix.classes.config.Configuration;
-import com.test.matrix.classes.mapping.DisplayMatrix;
-import java.io.*;
-import java.lang.reflect.InvocationTargetException;
+
 
 
 public class Main {
     public static void main(String[] args) {
 
-
-        try {
-
-            String configFilePath = "./resources/config.yaml";
-            Configuration ConfObj = new Configuration(configFilePath);
-            DisplayMatrix<Integer> DObj = new DisplayMatrix<>();
-            Calculator CObj = new Calculator(ConfObj.getGenRef());
-            CObj.addCmdList(ConfObj.getOpsList());
-            System.out.println("Result is");
-            DObj.displayMatrix(CObj.calculate());
-            System.out.println("Initial matrix is");
-            DObj.displayMatrix(CObj.getInitialMatrix());
-
-        } catch (IllegalArgumentException e1) {
-            System.out.println("Wrong argument " + e1.getMessage());
-        }  catch (ClassNotFoundException | NoSuchMethodException | InstantiationException |
-                 IllegalAccessException | InvocationTargetException | IOException e3) {
-            System.out.println(e3.getMessage());
-        }
 
 
 
